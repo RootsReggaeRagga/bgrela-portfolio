7487aa182803826b5695498db5d0d387b3494d76
bartlomiej.grela <bartlomiej.grela@gmail.com>
2024-01-09 14:55:47 +0100

    add Internationalization for Nuxt

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <h1>WHellow world</h1>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      title: "Bartłomiej Grela Portfolio"
+    }
+  },
+
+  modules: [
+    '@nuxtjs/i18n',
+  ],
+
 })

d7306cf15f54d91f2147eaea39fc0ba2cab2aa38
bartlomiej.grela <bartlomiej.grela@gmail.com>
2024-01-09 15:00:03 +0100

    Simple  Internationalization string test

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
     <h1>WHellow world</h1>
+    <p>{{ $t('welcome') }}</p>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,5 +13,8 @@ export default defineNuxtConfig({
   modules: [
     '@nuxtjs/i18n',
   ],
+  i18n: {
+    vueI18n: './i18n.config.ts' // if you are using custom path, default 
+  }
 
 })
